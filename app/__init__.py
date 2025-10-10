a4f35114b62a635e3e88f764e8f8921c0ec3b905
nikhxxt <nikhxtwork@gmail.com>
2025-10-10 19:58:14 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,1 @@
-# app/__init__.py
-
-# This file marks the 'app' directory as a Python package.
-# It can be left empty unless you want to expose specific modules or preload logic.
+uvicorn app.main:app
