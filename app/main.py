74bcb5b3a93dc8ca94e35412ccbaa205e5d055d8
nikhxxt <nikhxtwork@gmail.com>
2025-10-12 14:58:48 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,12 +1,6 @@
+# main.py
 from fastapi import FastAPI
-from app.routers import products
+from app.routes import products
 
 app = FastAPI()
-
-@app.get("/")
-def welcome():
-    return {"message": "Welcome to the E-commerce Product API!"}
-
 app.include_router(products.router)
-
-
