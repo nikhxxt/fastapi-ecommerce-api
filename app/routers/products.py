c333bc8d2840f23442a45e15c3cc3220f9c95cb0
nikhxxt <nikhxtwork@gmail.com>
2025-10-12 14:02:35 +0530

    Update products.py

--- app/routers/products.py
+++ app/routers/products.py
@@ -7,22 +7,8 @@ product_db = {}
 @router.post("/products/", response_model=ProductPublic)
 def create_product(product: ProductCreate):
     product_db[product.id] = product
-    return ProductPublic(id=product.id, name=product.name, price=product.price)
-
-@router.get("/products/", response_model=dict)
-def get_all_products():
-    return {
-        pid: {
-            "id": prod.id,
-            "name": prod.name,
-            "price": prod.price
-        }
-        for pid, prod in product_db.items()
-    }
-
-@router.get("/admin/products/", response_model=dict)
-def get_all_products_admin():
-    return {
-        pid: prod.dict()
-        for pid, prod in product_db.items()
-    }
+    return ProductPublic(
+        id=product.id,
+        name=product.name,
+        price=product.price
+    )
